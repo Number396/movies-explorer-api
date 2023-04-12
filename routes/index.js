0140a56ad4f0d2836b3e83f250bc4bd1decf66be
Number396 <number396@yandex.ru>
2023-04-12 18:07:52 +0300

    refactor: move celeb validation to the middleware

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,17 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
 const userRouter = require('./users');
 const movieRouter = require('./movies');
 const { login, createUser } = require('../controllers/users');
 const NotFoundError = require('../errors/not-found-err');
 const { auth } = require('../middlewares/auth');
 const { pageNotFound } = require('../errors/badUserResponces');
+const { signinValidation, signupValidation } = require('../middlewares/celebValidation');
 
 router.use('/users', userRouter);
 router.use('/movies', movieRouter);
 
-router.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
-
-router.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), createUser);
+router.post('/signin', signinValidation, login);
+router.post('/signup', signupValidation, createUser);
 
 router.use(auth);
 router.use((req, res, next) => {
