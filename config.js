498e8597f3002f8173a7a714aacb4849481a652a
Number396 <number396@yandex.ru>
2023-04-12 11:14:37 +0300

    refactor: change port

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 // eslint-disable-next-line no-useless-escape
 const { PATTERN = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/ } = process.env;
-const { PORT = '3000' } = process.env;
+const { PORT = '3001' } = process.env;
 const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 module.exports = {
