5ba89dbf9eb0d63380488803e08cea5006022b84
Number396 <number396@yandex.ru>
2023-04-12 11:23:43 +0300

    refactor: delete eslint comments

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-// eslint-disable-next-line no-useless-escape
+
 const { PATTERN = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/ } = process.env;
 const { PORT = '3001' } = process.env;
 const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
